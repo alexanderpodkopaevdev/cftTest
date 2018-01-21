f8fed8707f0bf8b0b42c594383117cde4cb85e17
Alexandr Podkopaev <p-alex1990@mail.ru>
2018-01-21 22:32:53 +0700

    0.3

--- src/main/java/org/podkopaev/FileSort.java
+++ src/main/java/org/podkopaev/FileSort.java
@@ -1,17 +1,16 @@
 package org.podkopaev;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
 
 /**
  * Hello world!
  */
 public class FileSort {
 
-    static boolean intOrString = false;
-    static boolean waningOrAscending = false;
+    private static boolean intOrString = false;
+    private static boolean waningOrAscending = false;
 
     public static void main(String[] args) {
         if (args.length != 4) {
@@ -22,7 +21,9 @@ public class FileSort {
         String param1 = args[2];
         String param2 = args[3];
         checkParam(param1,param2);
-
+        ArrayList<String> listElements = readFile(inputFileName);
+        String[] readyArray = sortArr(listElements);
+        writeFile(readyArray, outputFileName);
 
         //System.out.println( "Hello World!" );
     }
@@ -49,19 +50,97 @@ public class FileSort {
         } else if (param1.equals("-a")) {
             waningOrAscending = false;
         } else askQuestion("Неверно указан параметр для выбора режима сортировки. Использовать сортировку по возрастанию по-умолчанию или закрыть программу?");
-
     }
 
     private static void askQuestion(String text) {
-        System.out.println(text + ". Если хотите продолжить с параметом по-умолчанию введите \"Y\"");
+        System.out.println(text + "\n" + "Если хотите продолжить с параметом по-умолчанию введите \"Y\"");
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String answer = reader.readLine();
             if (!(answer.equals("Y"))) {
-                exitAfterPress("Параметр указан неверно");
+                exitAfterPress("Работа с программой окончена");
             }
         } catch (IOException ex) {
             exitAfterPress("Произошла ошибка ввода");
         }
-
     }
+
+    private static ArrayList<String> readFile(String fileName) {
+        ArrayList<String> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                list.add(reader.readLine());
+            }
+        } catch (FileNotFoundException e) {
+            exitAfterPress("Файл не найден");
+        }
+        catch (IOException ex) {
+            exitAfterPress("Ошибка чтения входного файла");
+        }
+        return list;
+    }
+
+    private static String[] sortArr(ArrayList<String> list) {
+        String[] readyArray = new String[list.size()];
+        if (intOrString) {
+            int[] arrayInt = new int[list.size()];
+            try {
+                for (int i = 0; i < list.size(); i++) {
+                    arrayInt[i] = Integer.parseInt(list.get(i));
+                }
+                readyArray = insertionSort(arrayInt);
+            } catch (NumberFormatException ex) {
+                askQuestion("Указанный файл содержит не только числа. По-умолчанию он будет отсортирован как строковый");
+            }
+        } else readyArray = insertionSort(list);
+        return readyArray;
+    }
+
+    private static String[] insertionSort(int[] array){
+
+        for (int i=1; i<array.length; ++i)
+        {
+            int key = array[i];
+            int j = i-1;
+            while (j>=0 &&(!waningOrAscending? array[j] > key : array[j] < key))
+            {
+                array[j+1] = array[j];
+                j = j-1;
+            }
+            array[j+1] = key;
+        }
+        String[] readyArray = new String[array.length];
+        for (int i : array) {
+            readyArray[i] = String.valueOf(array[i]);
+        }
+        return readyArray;
+    }
+
+    private static String[] insertionSort(ArrayList<String> list) {
+        for (int i=1; i<list.size(); ++i)
+        {
+            String key = list.get(i);
+            int j = i-1;
+            while (j>=0 &&(!waningOrAscending? list.get(j).compareTo(key) > 0 : list.get(j).compareTo(key) < 0))
+            {
+                list.set(j+1,list.get(j));
+                j = j-1;
+            }
+            list.set(j+1, key);
+        }
+        return list.toArray(new String[list.size()]);
+    }
+
+    private static void writeFile(String[] array, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String s : array) {
+                writer.write(s);
+            }
+        } catch (FileNotFoundException e) {
+            exitAfterPress("Файл не найден");
+        }
+        catch (IOException ex) {
+            exitAfterPress("Ошибка записил файла");
+        }
+    }
+
 }
