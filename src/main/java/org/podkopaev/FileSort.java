a3d52cbf708d108e0a60c2b9f01d14e9583a52fd
Alexandr Podkopaev <p-alex1990@mail.ru>
2018-01-22 23:33:09 +0700

    ready

--- src/main/java/org/podkopaev/FileSort.java
+++ src/main/java/org/podkopaev/FileSort.java
@@ -4,13 +4,11 @@ package org.podkopaev;
 import java.io.*;
 import java.util.ArrayList;
 
-/**
- * Hello world!
- */
 public class FileSort {
 
     private static boolean intOrString = false;
     private static boolean waningOrAscending = false;
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
         if (args.length != 4) {
@@ -20,14 +18,18 @@ public class FileSort {
         String outputFileName = args[1];
         String param1 = args[2];
         String param2 = args[3];
-        checkParam(param1,param2);
+        checkParam(param1, param2);
         ArrayList<String> listElements = readFile(inputFileName);
         String[] readyArray = sortArr(listElements);
         writeFile(readyArray, outputFileName);
-
-        //System.out.println( "Hello World!" );
+        try {
+            reader.close();
+        } catch (IOException e) {
+            exitAfterPress("Ошибка закрытия входного потока");
+        }
     }
 
+    //Выход из программы с указанием ошибки
     private static void exitAfterPress(String errorText) {
         String warningText = "Ошибка в программе, ознакомтесь с информацией и нажмите любую кнопку для выхода";
         System.out.println(warningText + "\n" + errorText);
@@ -39,22 +41,26 @@ public class FileSort {
         }
     }
 
+    //Проверка корректности параметров сортировка
     private static void checkParam(String param1, String param2) {
         if (param1.equals("-i")) {
             intOrString = true;
         } else if (param1.equals("-s")) {
             intOrString = false;
-        } else askQuestion("Неверно указан параметр для выбора чисел и строк. Использовать строки по-умолчанию или закрыть программу?");
+        } else
+            askQuestion("Неверно указан параметр для выбора чисел и строк. Использовать строки по-умолчанию или закрыть программу?");
         if (param2.equals("-d")) {
             waningOrAscending = true;
         } else if (param1.equals("-a")) {
             waningOrAscending = false;
-        } else askQuestion("Неверно указан параметр для выбора режима сортировки. Использовать сортировку по возрастанию по-умолчанию или закрыть программу?");
+        } else
+            askQuestion("Неверно указан параметр для выбора режима сортировки. Использовать сортировку по возрастанию по-умолчанию или закрыть программу?");
     }
 
+    //Вывод сообщений при предупреждениях
     private static void askQuestion(String text) {
         System.out.println(text + "\n" + "Если хотите продолжить с параметом по-умолчанию введите \"Y\"");
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try {
             String answer = reader.readLine();
             if (!(answer.equals("Y"))) {
                 exitAfterPress("Работа с программой окончена");
@@ -64,21 +70,22 @@ public class FileSort {
         }
     }
 
+    //Чтение входного файла
     private static ArrayList<String> readFile(String fileName) {
         ArrayList<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            while (reader.ready()) {
-                list.add(reader.readLine());
+        try (BufferedReader readerFile = new BufferedReader(new FileReader(fileName))) {
+            while (readerFile.ready()) {
+                list.add(readerFile.readLine());
             }
         } catch (FileNotFoundException e) {
-            exitAfterPress("Файл не найден");
-        }
-        catch (IOException ex) {
+            exitAfterPress("Исходный файл не найден");
+        } catch (IOException ex) {
             exitAfterPress("Ошибка чтения входного файла");
         }
         return list;
     }
 
+    //Проверка соответствия типа файлов и выбранных параметров
     private static String[] sortArr(ArrayList<String> list) {
         String[] readyArray = new String[list.size()];
         if (intOrString) {
@@ -95,18 +102,16 @@ public class FileSort {
         return readyArray;
     }
 
-    private static String[] insertionSort(int[] array){
-
-        for (int i=1; i<array.length; ++i)
-        {
+    //Сортировка числового файла
+    private static String[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; ++i) {
             int key = array[i];
-            int j = i-1;
-            while (j>=0 &&(!waningOrAscending? array[j] > key : array[j] < key))
-            {
-                array[j+1] = array[j];
-                j = j-1;
+            int j = i - 1;
+            while (j >= 0 && (!waningOrAscending ? array[j] > key : array[j] < key)) {
+                array[j + 1] = array[j];
+                j = j - 1;
             }
-            array[j+1] = key;
+            array[j + 1] = key;
         }
         String[] readyArray = new String[array.length];
         for (int i : array) {
@@ -115,32 +120,32 @@ public class FileSort {
         return readyArray;
     }
 
+    //Сортировка текстового файла
     private static String[] insertionSort(ArrayList<String> list) {
-        for (int i=1; i<list.size(); ++i)
-        {
+        for (int i = 1; i < list.size(); ++i) {
             String key = list.get(i);
-            int j = i-1;
-            while (j>=0 &&(!waningOrAscending? list.get(j).compareTo(key) > 0 : list.get(j).compareTo(key) < 0))
-            {
-                list.set(j+1,list.get(j));
-                j = j-1;
+            int j = i - 1;
+            while (j >= 0 && (!waningOrAscending ? list.get(j).compareTo(key) > 0 : list.get(j).compareTo(key) < 0)) {
+                list.set(j + 1, list.get(j));
+                j = j - 1;
             }
-            list.set(j+1, key);
+            list.set(j + 1, key);
         }
         return list.toArray(new String[list.size()]);
     }
 
+    //Запись данных в файл
     private static void writeFile(String[] array, String fileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             for (String s : array) {
                 writer.write(s);
+                writer.newLine();
             }
+            System.out.println("Файл отсортирован и записан");
         } catch (FileNotFoundException e) {
-            exitAfterPress("Файл не найден");
-        }
-        catch (IOException ex) {
+            exitAfterPress("Файл для записи не найден");
+        } catch (IOException ex) {
             exitAfterPress("Ошибка записил файла");
         }
     }
-
 }
--- src/main/java/org/podkopaev/InsertionSort.java
+++
@@ -1,4 +0,0 @@
-package org.podkopaev;
-
-public class InsertionSort {
-}
